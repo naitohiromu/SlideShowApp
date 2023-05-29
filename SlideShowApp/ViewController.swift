ba054c029f22a9ec04ecaa251c42fa45d4f6ac5c
naitohiromu <naito.hiromu01@genetec.co.jp>
2023-05-29 10:31:22 +0900

    Initial Commit

---
+++ SlideShowApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SlideShowApp
+//
+//  Created by naito.hiromu on 2023/05/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
